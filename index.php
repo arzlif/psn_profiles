ab44a6ec53399612e633bb770a1d770c7d27c218
arzlif <142876407+arzlif@users.noreply.github.com>
2023-08-22 10:46:08 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <title>PSN Database</title>
+    </head>
+    <body>
+        <h1>PSN profiles</h1>
+
+        <a href="./psn.php">Connected profiles</a>
+        <a href="./psne.php">Estonian's databases</a>
+    </body>
+</html>
